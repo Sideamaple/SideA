b06b5e1c696169e626f3bdc387eacc90d3c81a66
Maple <mr.Marlok2018@yandex.ru>
2021-08-01 21:44:07 +0400

    Initial commit

---
+++ Code/js/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.scroll').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        prevArrow: "<img src='../../1st%20Galery%20of%20Side%20A/Galery/Signs/Left-.svg' class='prev' alt='1'>",
+        nextArrow: "<img src='../../1st%20Galery%20of%20Side%20A/Galery/Signs/Right.svg' class='next' alt='2'>",
+
+    });
+});
