9bdd50a8797c59ebfe42cdc67c8c6836b11cf93c
Maple <mr.Marlok2018@yandex.ru>
2021-09-19 22:06:23 +0400

    Nothing

--- Code/slidergalery/js/sinon-button.js
+++ Code/slidergalery/js/sinon-button.js
@@ -7,7 +7,19 @@
 
  function sinondiv(id) {
      var div = document.getElementById(id);
-     div.style.display = div.style.display == "block" ? "none" : "block";
+     var overflowY = "hidden";
+
+     if (div.style.display == "block") {
+         div.style.display = "none";
+         document.body.style.overflowY = "hidden";
+     }
+        else {
+         div.style.display = "block"
+         document.body.style.overflowY = "show";
+     }
  }
+
+
+
 
 
